460e99a00f4962e0431ac7968f304d5d872d9fa6
Enzhi <zhouendeavor@gmail.com>
2022-03-24 00:37:31 +0800

    modify solveNQueens.cpp, add else{} in backTrack

--- src/Solution_solveNQueens.cpp
+++ src/Solution_solveNQueens.cpp
@@ -27,18 +27,20 @@ vector<vector<string> > Solution_solveNQueens(int n){
             vector<string> board = generateBoard(queens, n);
             solution.push_back(board);
         }
-        for (int col = 0; col < n; ++col){
-            if (columns.find(col) != columns.end() || diagonals1.find(row - col) != diagonals1.end() || diagonals2.find(row + col) != diagonals1.end()){
-                continue;
+        else{
+            for (int col = 0; col < n; ++col){
+                if (columns.find(col) != columns.end() || diagonals1.find(row - col) != diagonals1.end() || diagonals2.find(row + col) != diagonals1.end()){
+                    continue;
+                }
+                queens[row] = col;
+                columns.insert(col);
+                diagonals1.insert(row - col);
+                diagonals2.insert(row + col);
+                backTrack(row+1);
+                columns.erase(col);
+                diagonals1.erase(row - col);
+                diagonals2.erase(row + col);
             }
-            queens[row] = col;
-            columns.insert(col);
-            diagonals1.insert(row - col);
-            diagonals2.insert(row + col);
-            backTrack(row+1);
-            columns.erase(col);
-            diagonals1.erase(row - col);
-            diagonals2.erase(row + col);
         }
 
     };
